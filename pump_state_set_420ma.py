d80413c885a51ff5065ae7c44ec52da70b35495e
Charlie H. Shin <hwan.shin@gmail.com>
2022-06-19 23:47:16 +0900

    password 하드코딩 -> pv.password 사용, pylint 적용

--- pump_state_set_420ma.py
+++ pump_state_set_420ma.py
@@ -11,88 +11,199 @@ from pump_variables import PV
 #from pump_util import ThreadSafeSingleton
 from pump_btn import PumpButtons, buttons
 
+
 #class SetTimeStateMachine(metaclass=ThreadSafeSingleton):
 class SetSet420MAStateMachine():
-  states = [
-      { 'name':'ENTER_SET_LEVEL', 'on_enter':['enter_set_level'] }
-    , { 'name':'LEVEL_HIGH_4', 'on_enter':['high_4'] }
-    , { 'name':'LEVEL_HIGH_3', 'on_enter':['high_3'] }
-    , { 'name':'LEVEL_HIGH_2', 'on_enter':['high_2'] }
-    , { 'name':'LEVEL_HIGH_1', 'on_enter':['high_1'] }
-    , { 'name':'LEVEL_LOW_3', 'on_enter':['low_3'] }
-    , { 'name':'LEVEL_LOW_2', 'on_enter':['low_2'] }
-    , { 'name':'LEVEL_LOW_1', 'on_enter':['low_1'] }
-    , { 'name':'SAVE_LEVEL', 'on_enter':['save_level'] }
-    , { 'name':'DONT_SAVE_LEVEL', 'on_enter':['dont_save_level'] }
-    , { 'name':'LEAVE_SET_LEVEL', 'on_enter':['leave_set_level'] }
-  ]
+  states = [{
+      'name': 'ENTER_SET_LEVEL',
+      'on_enter': ['enter_set_level']
+  }, {
+      'name': 'LEVEL_HIGH_4',
+      'on_enter': ['high_4']
+  }, {
+      'name': 'LEVEL_HIGH_3',
+      'on_enter': ['high_3']
+  }, {
+      'name': 'LEVEL_HIGH_2',
+      'on_enter': ['high_2']
+  }, {
+      'name': 'LEVEL_HIGH_1',
+      'on_enter': ['high_1']
+  }, {
+      'name': 'LEVEL_LOW_3',
+      'on_enter': ['low_3']
+  }, {
+      'name': 'LEVEL_LOW_2',
+      'on_enter': ['low_2']
+  }, {
+      'name': 'LEVEL_LOW_1',
+      'on_enter': ['low_1']
+  }, {
+      'name': 'SAVE_LEVEL',
+      'on_enter': ['save_level']
+  }, {
+      'name': 'DONT_SAVE_LEVEL',
+      'on_enter': ['dont_save_level']
+  }, {
+      'name': 'LEAVE_SET_LEVEL',
+      'on_enter': ['leave_set_level']
+  }]
 
-  transitions = [
-      { 'source':'*', 'trigger':'enter', 'dest':'ENTER_SET_LEVEL'}
-    , { 'source':'ENTER_SET_LEVEL', 'trigger':'init', 'dest':'LEVEL_HIGH_4'}
-    , { 'source':'LEVEL_HIGH_4', 'trigger':'btn1_s', 'dest':'LEVEL_HIGH_3'}
-    , { 'source':'LEVEL_HIGH_3', 'trigger':'btn1_s', 'dest':'LEVEL_HIGH_2'}
-    , { 'source':'LEVEL_HIGH_2', 'trigger':'btn1_s', 'dest':'LEVEL_HIGH_1'}
-    , { 'source':'LEVEL_HIGH_1', 'trigger':'btn1_s', 'dest':'LEVEL_LOW_3'}
-    , { 'source':'LEVEL_LOW_3', 'trigger':'btn1_s', 'dest':'LEVEL_LOW_2'}
-    , { 'source':'LEVEL_LOW_2', 'trigger':'btn1_s', 'dest':'LEVEL_LOW_1'}
-    , { 'source':'LEVEL_LOW_1', 'trigger':'btn1_s', 'dest':'LEVEL_HIGH_4'}
-
-    , { 'source':'LEVEL_HIGH_4', 'trigger':'btn2_s', 'dest':'LEVEL_HIGH_4', 'before':'h_4_inc'}
-    , { 'source':'LEVEL_HIGH_4', 'trigger':'btn3_s', 'dest':'LEVEL_HIGH_4', 'before':'h_4_dec'}
-    , { 'source':'LEVEL_HIGH_3', 'trigger':'btn2_s', 'dest':'LEVEL_HIGH_3', 'before':'h_3_inc'}
-    , { 'source':'LEVEL_HIGH_3', 'trigger':'btn3_s', 'dest':'LEVEL_HIGH_3', 'before':'h_3_dec'}
-    , { 'source':'LEVEL_HIGH_2', 'trigger':'btn2_s', 'dest':'LEVEL_HIGH_2', 'before':'h_2_inc'}
-    , { 'source':'LEVEL_HIGH_2', 'trigger':'btn3_s', 'dest':'LEVEL_HIGH_2', 'before':'h_2_dec'}
-    , { 'source':'LEVEL_HIGH_1', 'trigger':'btn2_s', 'dest':'LEVEL_HIGH_1', 'before':'h_1_inc'}
-    , { 'source':'LEVEL_HIGH_1', 'trigger':'btn3_s', 'dest':'LEVEL_HIGH_1', 'before':'h_1_dec'}
-
-    , { 'source':'LEVEL_LOW_3', 'trigger':'btn2_s', 'dest':'LEVEL_LOW_3', 'before':'L_3_inc'}
-    , { 'source':'LEVEL_LOW_3', 'trigger':'btn3_s', 'dest':'LEVEL_LOW_3', 'before':'L_3_dec'}
-    , { 'source':'LEVEL_LOW_2', 'trigger':'btn2_s', 'dest':'LEVEL_LOW_2', 'before':'L_2_inc'}
-    , { 'source':'LEVEL_LOW_2', 'trigger':'btn3_s', 'dest':'LEVEL_LOW_2', 'before':'L_2_dec'}
-    , { 'source':'LEVEL_LOW_1', 'trigger':'btn2_s', 'dest':'LEVEL_LOW_1', 'before':'L_1_inc'}
-    , { 'source':'LEVEL_LOW_1', 'trigger':'btn3_s', 'dest':'LEVEL_LOW_1', 'before':'L_1_dec'}
-
-    , { 'source':'*', 'trigger':'btn1_l', 'dest':'SAVE_OR_NOT'}
-    , { 'source':'SAVE_OR_NOT', 'trigger':'btn2_s', 'dest':'SAVE_LEVEL'}
-    , { 'source':'SAVE_OR_NOT', 'trigger':'btn3_s', 'dest':'DONT_SAVE_LEVEL'}
-    , { 'source':'*', 'trigger':'leave', 'dest':'LEAVE_SET_LEVEL'}
-  ]
+  transitions = [{
+      'source': '*',
+      'trigger': 'enter',
+      'dest': 'ENTER_SET_LEVEL'
+  }, {
+      'source': 'ENTER_SET_LEVEL',
+      'trigger': 'init',
+      'dest': 'LEVEL_HIGH_4'
+  }, {
+      'source': 'LEVEL_HIGH_4',
+      'trigger': 'btn1_s',
+      'dest': 'LEVEL_HIGH_3'
+  }, {
+      'source': 'LEVEL_HIGH_3',
+      'trigger': 'btn1_s',
+      'dest': 'LEVEL_HIGH_2'
+  }, {
+      'source': 'LEVEL_HIGH_2',
+      'trigger': 'btn1_s',
+      'dest': 'LEVEL_HIGH_1'
+  }, {
+      'source': 'LEVEL_HIGH_1',
+      'trigger': 'btn1_s',
+      'dest': 'LEVEL_LOW_3'
+  }, {
+      'source': 'LEVEL_LOW_3',
+      'trigger': 'btn1_s',
+      'dest': 'LEVEL_LOW_2'
+  }, {
+      'source': 'LEVEL_LOW_2',
+      'trigger': 'btn1_s',
+      'dest': 'LEVEL_LOW_1'
+  }, {
+      'source': 'LEVEL_LOW_1',
+      'trigger': 'btn1_s',
+      'dest': 'LEVEL_HIGH_4'
+  }, {
+      'source': 'LEVEL_HIGH_4',
+      'trigger': 'btn2_s',
+      'dest': 'LEVEL_HIGH_4',
+      'before': 'h_4_inc'
+  }, {
+      'source': 'LEVEL_HIGH_4',
+      'trigger': 'btn3_s',
+      'dest': 'LEVEL_HIGH_4',
+      'before': 'h_4_dec'
+  }, {
+      'source': 'LEVEL_HIGH_3',
+      'trigger': 'btn2_s',
+      'dest': 'LEVEL_HIGH_3',
+      'before': 'h_3_inc'
+  }, {
+      'source': 'LEVEL_HIGH_3',
+      'trigger': 'btn3_s',
+      'dest': 'LEVEL_HIGH_3',
+      'before': 'h_3_dec'
+  }, {
+      'source': 'LEVEL_HIGH_2',
+      'trigger': 'btn2_s',
+      'dest': 'LEVEL_HIGH_2',
+      'before': 'h_2_inc'
+  }, {
+      'source': 'LEVEL_HIGH_2',
+      'trigger': 'btn3_s',
+      'dest': 'LEVEL_HIGH_2',
+      'before': 'h_2_dec'
+  }, {
+      'source': 'LEVEL_HIGH_1',
+      'trigger': 'btn2_s',
+      'dest': 'LEVEL_HIGH_1',
+      'before': 'h_1_inc'
+  }, {
+      'source': 'LEVEL_HIGH_1',
+      'trigger': 'btn3_s',
+      'dest': 'LEVEL_HIGH_1',
+      'before': 'h_1_dec'
+  }, {
+      'source': 'LEVEL_LOW_3',
+      'trigger': 'btn2_s',
+      'dest': 'LEVEL_LOW_3',
+      'before': 'L_3_inc'
+  }, {
+      'source': 'LEVEL_LOW_3',
+      'trigger': 'btn3_s',
+      'dest': 'LEVEL_LOW_3',
+      'before': 'L_3_dec'
+  }, {
+      'source': 'LEVEL_LOW_2',
+      'trigger': 'btn2_s',
+      'dest': 'LEVEL_LOW_2',
+      'before': 'L_2_inc'
+  }, {
+      'source': 'LEVEL_LOW_2',
+      'trigger': 'btn3_s',
+      'dest': 'LEVEL_LOW_2',
+      'before': 'L_2_dec'
+  }, {
+      'source': 'LEVEL_LOW_1',
+      'trigger': 'btn2_s',
+      'dest': 'LEVEL_LOW_1',
+      'before': 'L_1_inc'
+  }, {
+      'source': 'LEVEL_LOW_1',
+      'trigger': 'btn3_s',
+      'dest': 'LEVEL_LOW_1',
+      'before': 'L_1_dec'
+  }, {
+      'source': '*',
+      'trigger': 'btn1_l',
+      'dest': 'SAVE_OR_NOT'
+  }, {
+      'source': 'SAVE_OR_NOT',
+      'trigger': 'btn2_s',
+      'dest': 'SAVE_LEVEL'
+  }, {
+      'source': 'SAVE_OR_NOT',
+      'trigger': 'btn3_s',
+      'dest': 'DONT_SAVE_LEVEL'
+  }, {
+      'source': '*',
+      'trigger': 'leave',
+      'dest': 'LEAVE_SET_LEVEL'
+  }]
 
   def __init__(self, *args, **kwargs):
     self.name = kwargs.get('name')
     self.pv = kwargs.get('pv')
     self.sm_locked_cls = MachineFactory.get_predefined(locked=True)
-    self.machine = self.sm_locked_cls(model=self, 
-    #self.machine = Machine(model=self, 
-      states=Set420MAStateMachine.states, 
-      transitions=Set420MAStateMachine.transitions,
-      initial='ENTER_SET_LEVEL',
-      ignore_invalid_triggers=True)
+    self.machine = self.sm_locked_cls(
+        model=self,
+        #self.machine = Machine(model=self,
+        states=Set420MAStateMachine.states,
+        transitions=Set420MAStateMachine.transitions,
+        initial='ENTER_SET_LEVEL',
+        ignore_invalid_triggers=True)
 
-    print("Set420MAStateMachine.__init__(pv:",self.pv)
+    print("Set420MAStateMachine.__init__(pv:", self.pv)
 
-    self.high_old = [0,0,0,0]
-    self.low_old = [0,0,0]
+    self.high_old = [0, 0, 0, 0]
+    self.low_old = [0, 0, 0]
 
-    self.high = [0,0,0,0]
-    self.low = [0,0,0]
+    self.high = [0, 0, 0, 0]
+    self.low = [0, 0, 0]
 
   def enter_set_level(self):
-    print("enter_set_level:",self)
+    print("enter_set_level:", self)
     buttons().set_statemachine(self)
-    self.high_old = [ 
-        self.pv.setting_high//1000
-      , (self.pv.setting_high//100)%10
-      , (self.pv.setting_high//10)%10
-      , self.pv.setting_high%10
-    ] 
-    self.low_old = [ 
-        self.pv.setting_low//100
-      , (self.pv.setting_low//10)%10
-      , self.pv.setting_low%10
-    ] 
+    self.high_old = [
+        self.pv.setting_high // 1000, (self.pv.setting_high // 100) % 10,
+        (self.pv.setting_high // 10) % 10, self.pv.setting_high % 10
+    ]
+    self.low_old = [
+        self.pv.setting_low // 100, (self.pv.setting_low // 10) % 10,
+        self.pv.setting_low % 10
+    ]
     self.high = self.high_old.copy()
     self.low = self.low_old.copy()
     scr_enter_set_level(self.pv)
@@ -100,44 +211,44 @@ class SetSet420MAStateMachine():
 
   def save_or_not(self):
     print("callback: save_or_not", self)
-    if self.y_old!=self.y or self.m_old!=self.m or self.d_old!=self.d or self.h_old!=self.h or self.min_old!=self.min:
+    if self.y_old != self.y or self.m_old != self.m or self.d_old != self.d or self.h_old != self.h or self.min_old != self.min:
       scr_setting_save_or_not(self.pv)
     else:
       self.leave()
 
   def save_time(self):
     ts = f"{self.m}/{self.d}/{self.y} {self.h}:{self.min}"
-    print(f"callback: save_time:{ts}",self)
+    print(f"callback: save_time:{ts}", self)
     c = pexpect.spawn(f"sudo /usr/bin/date --set='{ts}'")
     try:  # 현재 user id에 따라서 sudo password prompt가 안나올 수도 있음
       c.expect('password')
-      c.sendline('rudakwkd')
+      c.sendline(self.pv.password)
       print(c.read())
     except:
       pass
     self.leave()
 
   def dont_save_time(self):
-    print("callback: dont_save_time",self)
+    print("callback: dont_save_time", self)
     self.leave()
 
   def leave_time_setting(self):
-    print("callback: leave_time_setting",self)
+    print("callback: leave_time_setting", self)
     buttons().restore_statemachine()
 
   def check_leap(self):
-    if self.m==2 and self.d>28:
-      self.d=29
-      if calendar.isleap(self.y)==False:
-        self.d=28
+    if self.m == 2 and self.d > 28:
+      self.d = 29
+      if calendar.isleap(self.y) == False:
+        self.d = 28
         scr_settime_d01(self.pv)
 
   def check_date(self):
-    if self.m==2:
+    if self.m == 2:
       self.check_leap()
-    elif self.d==31 and self.m in [4,6,9,11]:
-        self.d=30
-        scr_settime_d01(self.pv)
+    elif self.d == 31 and self.m in [4, 6, 9, 11]:
+      self.d = 30
+      scr_settime_d01(self.pv)
 
   def high_4(self):
     print("callback: high_4")
@@ -174,10 +285,10 @@ class SetSet420MAStateMachine():
     return sum(d * 10**i for i, d in enumerate(lst[::-1]))
 
   def adjust_digit(v, amount=1):
-    v = v+amount
-    if v<0:
-      v = 9 
-    elif v>9:
+    v = v + amount
+    if v < 0:
+      v = 9
+    elif v > 9:
       v = 0
 
     return v
