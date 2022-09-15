f815ca01f4f5b0a402bc4e3e7bd9523d1cb06799
Hyunghwan Shin <hwan.shin@gmail.com>
2022-09-16 05:10:32 +0900

    refactoring

--- fan_control.py
+++ fan_control.py
@@ -8,8 +8,8 @@ import pump_util as util
 
 from gpiozero import OutputDevice
 
-ON_THRESHOLD = 50
-OFF_THRESHOLD = 48
+ON_THRESHOLD = 60
+OFF_THRESHOLD = 50 
 
 SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
 
@@ -40,11 +40,12 @@ def fan_proc(**kwargs):
         if temp > ON_THRESHOLD and not fan.value:
             logger.info(f"Temperature: {temp} Turn On the Fan")
             fan.on()
-            print("Fan On")
         elif fan.value and temp<OFF_THRESHOLD:
             logger.info(f"Temperature: {temp} Turn Off the Fan")
             fan.off()
-            print("Fan Off")
+        else:
+            logger.info(f"Temperature: {temp} Continue Fan State")
+          
 
         time.sleep(SLEEP_INTERVAL)
 
