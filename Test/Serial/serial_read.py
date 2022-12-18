06dc37244139fd6fa3470a19ff275917657a0cee
Hyunghwan Shin <hwan.shin@gmail.com>
2022-12-19 01:11:53 +0900

    refactor

--- Test/Serial/serial_read.py
+++ Test/Serial/serial_read.py
@@ -3,22 +3,24 @@
 import time
 import serial
 
-#o port = '/dev/serial1'
-port = '/dev/serial2'
+port = '/dev/serial1'
+#port = '/dev/serial2'
 #o port = '/dev/ttyAMA0'
 #x port = '/dev/ttyAMA1'
-ser = serial.Serial(
-    port,
-    baudrate = 115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=3
-)
+
+ser = serial.Serial(port,
+                    baudrate=115200,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=3)
 
 while 1:
-    #x=ser.readline()
-    x=ser.read(3)
-    if len(x)>1:
-       print(x)
-    time.sleep(2)
+  #x=ser.readline()
+  x = ser.read(3)
+  if len(x) > 1:
+    print(x)
+  else:
+    print("Nothing received\n")
+
+  time.sleep(2)
--- Test/Serial/serial_write.py
+++ Test/Serial/serial_write.py
@@ -1,7 +1,7 @@
 import time
 import serial
 
-ser = serial.Serial(
+ser1 = serial.Serial(
     #port='/dev/ttyAMA0',
     port='/dev/serial1',
     baudrate = 115200,
@@ -11,11 +11,26 @@ ser = serial.Serial(
     timeout=5
 )
 
+ser2 = serial.Serial(
+    #port='/dev/ttyAMA1',
+    port='/dev/serial2',
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=5
+)
+c=0
 while 1:
+    msg = f'serial#{c}'
+    msg = bytes(msg, 'utf-8')
+    ser1.write(b'serial')
+    print(f"message {c}")
+    c += 1
+
     #var = input(">>: ")
     #s = var.strip()
     #print("you entered ", s.encode('ASCII'))
     #ser.write("%s\r"%var)
     #ser.write(b"abc\r")
-    ser.write(b"abc")
-    time.sleep(2)
+    time.sleep(1)
