3bf3f3d59f85598e60679beef4d36ccd6ac95928
Hyunghwan Shin <hwan.shin@gmail.com>
2023-01-22 13:34:31 +0900

    pump.sh

--- pump.sh
+++ pump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 /home/smtech/pump/pump.py
+python3 /home/hwan/pump/pump.py
 #python3 /home/hwan/pump/pump.py >> /home/hwan/pump/logs/stream.log 2>&1
 #python3 /home/hwan/pump/pump.py >> /home/hwan/pump/pump.log 2>&1
